1f1c5a142762e1e9952ee6d4a12a65196aaca010
matitoma80 <matitoma2005@gmail.com>
2026-09-08 08:55:03 -0300

    resuelvo recorrido_misterioso

--- recorrido_misterioso.java
+++ recorrido_misterioso.java
@@ -1,49 +1,77 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Queue;
 
 public class recorrido_misterioso {
     public String recorrido_misterioso() {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-
         int cant_nodos = sc.nextInt();
 
-        int[][] aristas = new int[cant_nodos-1][2];
+        //creo las estructuras de datos
+        ArrayList<ArrayList<Integer>> conexiones = new ArrayList<>();
         int[] secuencia = new int[cant_nodos];
+        int[] posiciones = new int[cant_nodos + 1];
+        Queue<Integer> cola = new LinkedList<>();
+        boolean[] visitado = new boolean[cant_nodos + 1];
 
-        //pongo todas las aristas
-        for (int i = 0; i <= cant_nodos-1; i++){
-            aristas[i][0] = sc.nextInt();
-            aristas[i][1] = sc.nextInt();
+        //inicializo conexiones
+        for (int i = 0; i <= cant_nodos; i++){
+            conexiones.add(new ArrayList<>());
+        }
+
+        //pongo todas las conexiones
+        int v;
+        int u;
+        for (int i = 1; i <= cant_nodos - 1; i++){
+            v = sc.nextInt();
+            u = sc.nextInt();
+            conexiones.get(v).add(u);
+            conexiones.get(u).add(v);
         }
 
         //pongo la secuencia
-        for (int j = 0; j <= cant_nodos; j++){
+        for (int j = 0; j < cant_nodos; j++){
             secuencia[j] = sc.nextInt();
         }
 
-        //recorro la secuencia
-        for (int k = 0; k <= cant_nodos; k++){
-            //recorro las aristas
-            ArrayList<Integer> conexiones = new ArrayList<>();
-            boolean aparece_conexion = false;
-            for (int l = 0; l <= cant_nodos-1; l++){
-                if (secuencia[k] == aristas[l][0])
-                    conexiones.add(aristas[l][1]);
-                else if (secuencia[k] == aristas[l][1])
-                    conexiones.add(aristas[l][0]);
-            }
-            if (conexiones.size() != 0 && k < cant_nodos)
-                for (int m = 0; m <= conexiones.size(); m++){
-                    if (conexiones.get(m) == secuencia[k])
-                        aparece_conexion = true;
-                }
-                if (aparece_conexion == false)
-                    return "No";
+        if (secuencia[0] != 1) {
+            return "No";
         }
 
+        //pongo posiciones
+        for (int k = 0; k < cant_nodos; k++){
+            posiciones[secuencia[k]] = k;
+        }
+    
+        for (int i = 1; i <= cant_nodos; i++) {
+            Collections.sort(conexiones.get(i), (a, b) -> Integer.compare(posiciones[a], posiciones[b]));
+        }
+
+        //encolo el nodo 1
+        cola.add(1);
+        visitado[1] = true;
+        int idx = 0;
+
+        while (!cola.isEmpty()) {
+            int actual = cola.poll();
+
+            // Si el nodo que sale de la cola no coincide con la posición actual en secuencia:
+            if (secuencia[idx++] != actual) {
+                return "No";
+            }
+
+            // Encolamos los vecinos no visitados
+            for (int vecino : conexiones.get(actual)) {
+                if (!visitado[vecino]) {
+                    visitado[vecino] = true;
+                    cola.add(vecino);
+                }
+            }
+        }
         return "yes";
     }
 }
